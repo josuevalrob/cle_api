66646d68dcdc1bf34701ecb690fbdde316618504
josue valencia <josue.valrob@gmail.com>
2020-02-22 12:55:39 +0100

    refactor package.json

--- src/routes/root.route.js
+++ src/routes/root.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.send('Building Site')
+    res.send('🪓👷🏻‍♂️🧰🛠👷🏻‍♀️🏗')
 })
 
 export default router
