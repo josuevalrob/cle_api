2dbd5e315ac406568990a1de15e8001647ffb23e
josue valencia <josue.valrob@gmail.com>
2020-08-19 10:54:17 +0200

    first testing gampings

--- src/resolvers/camping/camping.query.test.js
+++ src/resolvers/camping/camping.query.test.js
@@ -15,26 +15,33 @@ test("fetch all Guests from all turnos", async (done) => {
   agent.post("/graphql").send({
     query: `{ getTurnos { id } }` //* return all id from all turnos
   }).set("Accept", "application/json")
+    .expect(200)
     .end((err, {body:{data:{getTurnos}}}) => {
       if (err) return done(err);
 
       getTurnos.map(({id}) => {
-        console.log(id)
-        agent.post("/graphql").query({
-          query: `{ //* Return all the fields from that specific turno id
-            getCampings (input:{ turno: "${id}" }) {
+        agent.post("/graphql").send({
+          //* get all campings from that turno
+          query: `{
+            getCampings(input:{
+              turno: "${id}"
+            }) {
               id
+              turno { id }
+              owner { id  }
+              patreon { id }
+              guest { id }
             }
           }`
-        }).set('Content-Type', "application/json")
-          .end((err, response) => {
+        }).set("Accept", "application/json")
+          .expect(200)
+          .end((err, {body:{data}}) => {
             if (err) return done(err);
             // * TESTS:
-            console.log(response.error)
-            // expect(data).toBeInstanceOf(Object);
-            // const {getCampings} = data;
-            // expect(getCampings).toBeInstanceOf(Object);
-            // CampingTypes.map(checkFields(getCampings))
+            expect(data).toBeInstanceOf(Object);
+            const {getCampings} = data;
+            expect(getCampings).toBeInstanceOf(Array);
+            getCampings.map(({turno}) => expect(turno.id).toBe(id));
             done();
           });
       })
