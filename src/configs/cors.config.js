ef9a0fd1489546e3a60c3729734c6cb7c89c2afc
josue valencia <josue.valrob@gmail.com>
2021-01-09 11:31:01 +0100

    improve cors config

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -2,5 +2,11 @@ const cors = require('cors')
 
 module.exports = cors({
   credentials: true,
-  origin: process.env.ALLOW_ORIGINS
+  origin: (origin, callback) => {
+    if (process.env.ALLOW_ORIGINS === origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 })
